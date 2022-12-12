0902488deb569a46a360ffdc43e98bfd662afa71
Andy Schneider <andy.schneider@shopify.com>
2022-12-12 08:33:36 -0700

    Updated part 2 to kind of just run the algo for part 1 in reverse

--- 2022/12.rb
+++ 2022/12.rb
@@ -14,7 +14,6 @@ module Day12
             start = [row, col]
             puts "START POINT: [#{row}, #{col}]"
             break
-            # steps[row][col] = 0
           end
         }
       }
@@ -28,18 +27,17 @@ module Day12
         }
       }
 
-      explore(input, steps, start[0], start[1], 0)
+      explore(input, steps, start[0], start[1], 0, "E")
 
       # Might need to search steps for the value at E
       return steps[end_pos[0]][end_pos[1]]
     end
 
-    def explore(input, steps, row, col, current_steps)
-      # if the square we've arrived at is "E"
+    def explore(input, steps, row, col, current_steps, end_val)
       val = input[row][col]
-      next_valid = val.next
-      if val == "E"
-        # puts "got to E, #{current_steps}"
+      # if the square we've arrived at is an end
+      if val == end_val
+        # puts "got to end, #{current_steps}"
         # update the steps value here, then return current_steps (???)
         if current_steps < steps[row][col]
           steps[row][col] = current_steps
@@ -50,54 +48,24 @@ module Day12
       # Try to set the number of steps it took to get here
       # if this is a new fastest way to get here, update, keep exploring
       if current_steps < steps[row][col]
-        if input[row][col] == @best.next
-          @best = input[row][col]
-        end
-        if input[row][col] == @best
-          # puts "Got to #{@best} in #{current_steps}"
-        end
         steps[row][col] = current_steps
       else
         return
       end
 
-      # Can we explore up?
-      if row > 0
-        new_row = row - 1
-        new_col = col
-        if ok_to_move(val, input[new_row][new_col]) && steps[new_row][new_col] > (current_steps)
-          # puts "Going up to #{input[new_row][new_col]} at [#{new_row}, #{new_col}]"
-          explore(input, steps, new_row, new_col, current_steps + 1)
-        end
-      end
-
-      # Down?
-      if row < input.length - 1
-        new_row = row + 1
-        new_col = col
-        if ok_to_move(val, input[new_row][new_col]) && steps[new_row][new_col] > (current_steps)
-          # puts "Going down to #{input[new_row][new_col]} at [#{new_row}, #{new_col}]"
-          explore(input, steps, new_row, new_col, current_steps + 1)
-        end
-      end
-
-      # Left?
-      if col > 0
-        new_row = row
-        new_col = col - 1
-        if ok_to_move(val, input[new_row][new_col]) && steps[new_row][new_col] > (current_steps)
-          # puts "Going left to #{input[new_row][new_col]} at [#{new_row}, #{new_col}]"
-          explore(input, steps, new_row, new_col, current_steps + 1)
-        end
-      end
-
-      # Right?
-      if col < input[row].strip.length - 1
-        new_row = row
-        new_col = col + 1
-        if ok_to_move(val, input[new_row][new_col]) && steps[new_row][new_col] > (current_steps)
-          # puts "Going right to #{input[new_row][new_col]} at [#{new_row}, #{new_col}]"
-          explore(input, steps, new_row, new_col, current_steps + 1)
+      new_poss = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]]
+      new_poss.each do |new_pos|
+        new_row = new_pos[0]
+        new_col = new_pos[1]
+        if new_row >= 0 && new_row < input.length &&
+           new_col >= 0 && new_col < input[new_row].strip.length
+          move_ok = ok_to_move(val, input[new_row][new_col])
+          if end_val == "a"
+            move_ok = ok_to_move(input[new_row][new_col], val)
+          end
+          if move_ok && steps[new_row][new_col] > (current_steps)
+            explore(input, steps, new_row, new_col, current_steps + 1, end_val)
+          end
         end
       end
     end
@@ -118,29 +86,35 @@ module Day12
     end
 
     def part_two(input)
-      @best = "a"
-      shortest = 999999
       # Recreate a grid of equal size of the puzzle input,
       # containing the steps required to get to each square
-      start_points = []
+      x_size = input[0].strip.length
+      y_size = input.length
+      steps = Array.new(y_size) { Array.new(x_size, 999999) }
+      start = []
       0.step(input.length - 1, 1) { |row|
         0.step(input[row].length - 1, 1) { |col|
-          if input[row][col] == "S" || input[row][col] == "a"
+          if input[row][col] == "S"
             input[row][col] = "a"
-            start_points.append([row, col])
+          end
+          if input[row][col] == "E"
+            start = [row, col]
           end
         }
       }
-      puts input
 
-      start_points.each do |start|
-        input_copy = Marshal.load(Marshal.dump(input))
-        input_copy[start[0]][start[1]] = "S"
-        result = part_one(input_copy)
-        if result < shortest
-          shortest = result
-        end
-      end
+      explore(input, steps, start[0], start[1], 0, "a")
+
+      shortest = 999999
+      0.step(input.length - 1, 1) { |row|
+        0.step(input[row].length - 1, 1) { |col|
+          if input[row][col] == "a"
+            if steps[row][col] < shortest
+              shortest = steps[row][col]
+            end
+          end
+        }
+      }
 
       return shortest
     end
@@ -148,8 +122,8 @@ module Day12
 end
 
 lines = File.readlines("examples/12/12.txt")
-# puts Day12.part_one(lines)
+puts Day12.part_one(lines)
 puts Day12.part_two(lines)
 lines = File.readlines("inputs/12.txt")
-# puts Day12.part_one(lines)
+puts Day12.part_one(lines)
 puts Day12.part_two(lines)
