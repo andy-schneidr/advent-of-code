dfbd4c3f1c5e7b2a3d5fdfc9989657c71d36531f
Andy Schneider <andy.schneider@shopify.com>
2023-12-06 08:17:25 -0700

    Improvements to day 6 :)

--- 2023/aoc-2023/src/components/useDay6/useDay6.tsx
+++ 2023/aoc-2023/src/components/useDay6/useDay6.tsx
@@ -1,25 +1,14 @@
 const waysToWin = (time: number, dist: number): number => {
-  let startOfWinning = -1;
-  let endOfWinning = -1;
   for (let i = 0; i <= time; i++) {
     // hold the button down for i seconds to get a speed of i
     // then let the boat travel at that speed for time - i seconds to get distance
-    // if the distance is equal to the target distance, increment ways
     const distance = (time - i) * i;
     if (distance > dist) {
-      startOfWinning = i;
-      break;
+      // Oooooh big math
+      return time + 1 - i * 2;
     }
   }
-  for (let i = time; i >= startOfWinning; i--) {
-    const distance = (time - i) * i;
-    if (distance > dist) {
-      endOfWinning = i;
-      break;
-    }
-  }
-  const ways = endOfWinning - startOfWinning + 1;
-  return ways;
+  return -1;
 };
 
 const part1 = (input: string[]): string | number => {
