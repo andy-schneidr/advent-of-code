a59ba42df87f3f0f4d3ff1f3e788df2ccdf9ad93
Andy Schneider <andy.schneider@shopify.com>
2023-12-14 00:34:14 -0700

    Clean up day 14, still not the best yeugh

--- 2023/aoc-2023/src/components/useDay14/useDay14.tsx
+++ 2023/aoc-2023/src/components/useDay14/useDay14.tsx
@@ -36,46 +36,6 @@ const firstEmptySpaceNorthFromPos = (
   return 0;
 };
 
-const firstEmptySpaceSouthFromPos = (
-  platform: number[][],
-  row: number,
-  column: number
-): number => {
-  for (let i = row + 1; i < platform.length; i++) {
-    if (platform[i][column] !== empty) {
-      return i - 1;
-    }
-  }
-  return platform.length - 1;
-};
-
-// West is <<<<<<, remember?
-const firstEmptySpaceWestFromPos = (
-  platform: number[][],
-  row: number,
-  column: number
-): number => {
-  for (let i = column - 1; i >= 0; i--) {
-    if (platform[row][i] !== empty) {
-      return i + 1;
-    }
-  }
-  return 0;
-};
-
-const firstEmptySpaceEastFromPos = (
-  platform: number[][],
-  row: number,
-  column: number
-): number => {
-  for (let i = column + 1; i < platform[row].length; i++) {
-    if (platform[row][i] !== empty) {
-      return i - 1;
-    }
-  }
-  return platform[row].length - 1;
-};
-
 const rollNorth = (platform: number[][]) => {
   for (let i = 0; i < platform.length; i++) {
     for (let j = 0; j < platform[i].length; j++) {
@@ -88,47 +48,24 @@ const rollNorth = (platform: number[][]) => {
   }
 };
 
-const rollWest = (platform: number[][]) => {
-  for (let col = 0; col < platform[0].length; col++) {
-    for (let row = 0; row < platform.length; row++) {
-      if (platform[row][col] === round) {
-        const newCol = firstEmptySpaceWestFromPos(platform, row, col);
-        platform[row][col] = empty;
-        platform[row][newCol] = round;
-      }
+const rotate90DegClockwise = (platform: number[][]): number[][] => {
+  const newPlatform: number[][] = [];
+  for (let i = 0; i < platform[0].length; i++) {
+    let row: number[] = [];
+    for (let j = platform.length - 1, o = 0; j >= 0; j--, o++) {
+      row.push(platform[j][i]);
     }
+    newPlatform.push(row);
   }
-};
-
-const rollSouth = (platform: number[][]) => {
-  for (let row = platform.length - 1; row >= 0; row--) {
-    for (let col = 0; col < platform[row].length; col++) {
-      if (platform[row][col] === round) {
-        const newRow = firstEmptySpaceSouthFromPos(platform, row, col);
-        platform[row][col] = empty;
-        platform[newRow][col] = round;
-      }
-    }
-  }
-};
-
-const rollEast = (platform: number[][]) => {
-  for (let col = platform[0].length - 1; col >= 0; col--) {
-    for (let row = 0; row < platform.length; row++) {
-      if (platform[row][col] === round) {
-        const newCol = firstEmptySpaceEastFromPos(platform, row, col);
-        platform[row][col] = empty;
-        platform[row][newCol] = round;
-      }
-    }
-  }
+  return newPlatform;
 };
 
 const runCycle = (platform: number[][]) => {
-  rollNorth(platform);
-  rollWest(platform);
-  rollSouth(platform);
-  rollEast(platform);
+  for (let i = 0; i < 4; i++) {
+    rollNorth(platform);
+    platform = rotate90DegClockwise(platform);
+  }
+  return platform;
 };
 
 const computeWeight = (platform: number[][]): number => {
@@ -164,7 +101,6 @@ const findCycle = (
             break;
           }
         }
-
         if (isCycle) {
           return { cycleStart, cycleEnd };
         }
@@ -181,12 +117,12 @@ const part1 = (input: string[]): string | number => {
 };
 
 const part2 = (input: string[]): string | number => {
-  const platform = convertToNumbers(input);
+  let platform = convertToNumbers(input);
 
   const results = [];
   // IDK????? 250 is enough to find the cycle??
   for (let i = 0; i < 250; i++) {
-    runCycle(platform);
+    platform = runCycle(platform);
     const weight = computeWeight(platform);
     // console.log(weight);
     results.push(weight);
